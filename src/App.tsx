36a0b378f19cb7ae228f5e50049b3988f2201b7f
Zilan Fidan <zilann.fidann@gmail.com>
2026-02-17 09:44:56 +0300

    chore:initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Web tasarımı ve programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: ZİLAN FİDAN</p>
+      <p>Öğrenci No: 230541143</p>
+    </div>
+  );
+}
+
+export default App;
